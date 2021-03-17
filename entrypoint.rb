ce4fa79472e77576e426ba9f87c12fd66b495b63
Ramiro <ramiro@okteto.com>
2021-03-17 15:50:10 -0700

    fix script
    Signed-off-by: Ramiro <ramiro@okteto.com>

--- entrypoint.rb
+++ entrypoint.rb
@@ -6,24 +6,19 @@ if !ENV["GITHUB_TOKEN"]
     exit(1)
 end
 
-# todo: make message optional and get the value from an URL
-if ARGV[0].empty?
-    puts "Missing message."
-    exit(1)
-end
-
 if ENV["GITHUB_EVENT_NAME"] != "pull_request"
     puts "This action only supports pull_request events."
     exit(1)
 end
 
+message = ARGV[0]
+repo = ENV["GITHUB_REPOSITORY"]
+
 json = File.read(ENV.fetch("GITHUB_EVENT_PATH"))
 event = JSON.parse(json)
 pr = event["number"]
-repo = ENV["GITHUB_REPOSITORY"]
 
 github = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
-
 comments = github.issue_comments(repo, pr)
 exists = coms.find { |c| c["body"] == message }
 
